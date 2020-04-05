9d4c4ca8ff075631cdaf9ff4eed9388aa6f98add
manuelmejiasleiva <mejiasleivamanuel@gmail.com>
2020-04-05 17:12:25 +0200

    añadido nuevo artículo

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ if (workbox) { //workbox solo existe en el scope del serviceWorker
 
     // añadimos configuración de workbox
     workbox.core.setCacheNameDetails({
-        prefix: 'tallerSysmana2020-app',
+        prefix: 'manuelmejias-app',
         suffix: 'v1',
         precache: 'precache-cache',
         runtime: 'runtime-cache'
@@ -14,32 +14,13 @@ if (workbox) { //workbox solo existe en el scope del serviceWorker
     // cacheamos archivos html con estrategia CacheFirst
     workbox.routing.registerRoute(
         /(\w*[-]?[\.]?\w*)\.html$/,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'cacheFirstHtml',
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
+        workbox.strategies.staleWhileRevalidate()
     );
 
     // cacheamos los archivos de imágenes con la estrategia CacheFirst
     workbox.routing.registerRoute(
         /\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'cacheFirstHtmlImages',
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                }),
-            ],
-        })
+        workbox.strategies.staleWhileRevalidate()
     );
 
     // cacheamos todos los archivos del directorio assets con estrategia staleWhileRevalidate
@@ -51,51 +32,17 @@ if (workbox) { //workbox solo existe en el scope del serviceWorker
     // cacheamos el webmanifest con estrategia CacheFirst
     workbox.routing.registerRoute(
         /manifest.webmanifest/,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'cacheFirstHtmlManifest',
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
+        workbox.strategies.staleWhileRevalidate()
     );
 
     // cacheamos CDN de librerías y el CDN  de workbox con estrategia CacheFirst
     workbox.routing.registerRoute(
         /(https:\/\/unpkg.+)|(https:\/\/cdnjs.+)|(https:\/\/cdn.+)|(https:\/\/storage\.googleapis.+)/g,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'cacheFirstHtmlCDN',
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
+        workbox.strategies.staleWhileRevalidate()
     );
 
     // cacheamos CSS de AOS que por alguna razón no lo pilla del CDN con estrategia CacheFirst
     workbox.routing.registerRoute(
-        /https:\/\/unpkg.com\/aos@next\/dist\/aos.css/g,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'cacheFirstHtmlAOS',
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
+        workbox.strategies.staleWhileRevalidate()
     );
 }
