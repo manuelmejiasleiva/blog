b619d2b7bcf2ae4471b3fbfd42b41fdb1714f391
susomejias <jesusmejias.jm@gmail.com>
2020-04-05 15:44:47 +0200

    fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,101 @@
-// service-worker.js
+// importamos workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-// set names for both precache & runtime cache
-workbox.core.setCacheNameDetails({
-    prefix: 'my-blog',
-    suffix: 'v1',
-    precache: 'precache',
-    runtime: 'runtime-cache'
-});
+if (workbox) { //workbox solo existe en el scope del serviceWorker
 
-// let Service Worker take control of pages ASAP
-workbox.skipWaiting();
-workbox.clientsClaim();
+    // añadimos configuración de workbox
+    workbox.core.setCacheNameDetails({
+        prefix: 'tallerSysmana2020-app',
+        suffix: 'v1',
+        precache: 'precache-cache',
+        runtime: 'runtime-cache'
+    });
 
-// let Workbox handle our precache list
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+    // cacheamos archivos html con estrategia CacheFirst
+    workbox.routing.registerRoute(
+        /(\w*[-]?[\.]?\w*)\.html$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'cacheFirstHtml',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
 
-// use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    workbox.strategies.networkFirst()
-);
+    // cacheamos los archivos de imágenes con la estrategia CacheFirst
+    workbox.routing.registerRoute(
+        /\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'cacheFirstHtmlImages',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                }),
+            ],
+        })
+    );
 
-// use `cacheFirst` strategy for images
-workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    workbox.strategies.cacheFirst()
-);
+    // cacheamos todos los archivos del directorio assets con estrategia staleWhileRevalidate
+    workbox.routing.registerRoute(
+        /assets\/(video\/(\w*[-]?[\.]?\w*).mp4)|assets\/(js\/(\w*[-]?[\.]?\w*).js)|assets\/(css\/(\w*[-]?[\.]?\w*).css)/,
+        workbox.strategies.staleWhileRevalidate()
+    );
 
-// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-workbox.routing.registerRoute(
-    /^https:\/\/fonts\.googleapis\.com/,
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'google-fonts-stylesheets',
-    })
-);
+    // cacheamos el webmanifest con estrategia CacheFirst
+    workbox.routing.registerRoute(
+        /manifest.webmanifest/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'cacheFirstHtmlManifest',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
 
-// Cache the underlying font files with a cache-first strategy for 1 year.
-workbox.routing.registerRoute(
-    /^https:\/\/fonts\.gstatic\.com/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'google-fonts-webfonts',
-        plugins: [
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200],
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-                maxEntries: 30,
-            }),
-        ],
-    })
-);
+    // cacheamos CDN de librerías y el CDN  de workbox con estrategia CacheFirst
+    workbox.routing.registerRoute(
+        /(https:\/\/unpkg.+)|(https:\/\/cdnjs.+)|(https:\/\/cdn.+)|(https:\/\/storage\.googleapis.+)/g,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'cacheFirstHtmlCDN',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
 
-workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|webp|svg|css|js|json|webmanifest)$/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'static-resources',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 60,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-        ],
-    })
-);
-
-workbox.routing.registerRoute(
-    new RegExp('https://unpkg.com/aos@next/dist/aos.js'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'static-resources',
-    })
-);
-
-workbox.routing.registerRoute(
-    new RegExp('https://unpkg.com/aos@next/dist/aos.css'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'static-resources',
-    })
-);
+    // cacheamos CSS de AOS que por alguna razón no lo pilla del CDN con estrategia CacheFirst
+    workbox.routing.registerRoute(
+        /https:\/\/unpkg.com\/aos@next\/dist\/aos.css/g,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'cacheFirstHtmlAOS',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
+}
