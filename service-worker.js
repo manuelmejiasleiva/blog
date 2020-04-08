59e1465c6a9ae394ae41e973e6a6861f795291d5
manuelmejiasleiva <mejiasleivamanuel@gmail.com>
2020-04-09 01:32:28 +0200

    añadido nuevo artículo

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ if (workbox) { //workbox solo existe en el scope del serviceWorker
 
     // añadimos configuración de workbox
     workbox.core.setCacheNameDetails({
-        prefix: 'manuelmejias-app',
+        prefix: 'manuelmejias-blog-app',
         suffix: 'v1',
         precache: 'precache-cache',
         runtime: 'runtime-cache'
@@ -14,35 +14,30 @@ if (workbox) { //workbox solo existe en el scope del serviceWorker
     // cacheamos archivos html con estrategia CacheFirst
     workbox.routing.registerRoute(
         /(\w*[-]?[\.]?\w*)\.html$/,
-        workbox.strategies.staleWhileRevalidate()
+        workbox.strategies.StaleWhileRevalidate()
     );
 
     // cacheamos los archivos de imágenes con la estrategia CacheFirst
     workbox.routing.registerRoute(
         /\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/,
-        workbox.strategies.staleWhileRevalidate()
+        workbox.strategies.StaleWhileRevalidate()
     );
 
     // cacheamos todos los archivos del directorio assets con estrategia staleWhileRevalidate
     workbox.routing.registerRoute(
         /assets\/(video\/(\w*[-]?[\.]?\w*).mp4)|assets\/(js\/(\w*[-]?[\.]?\w*).js)|assets\/(css\/(\w*[-]?[\.]?\w*).css)/,
-        workbox.strategies.staleWhileRevalidate()
+        workbox.strategies.StaleWhileRevalidate()
     );
 
     // cacheamos el webmanifest con estrategia CacheFirst
     workbox.routing.registerRoute(
         /manifest.webmanifest/,
-        workbox.strategies.staleWhileRevalidate()
+        workbox.strategies.StaleWhileRevalidate()
     );
 
     // cacheamos CDN de librerías y el CDN  de workbox con estrategia CacheFirst
     workbox.routing.registerRoute(
         /(https:\/\/unpkg.+)|(https:\/\/cdnjs.+)|(https:\/\/cdn.+)|(https:\/\/storage\.googleapis.+)/g,
-        workbox.strategies.staleWhileRevalidate()
-    );
-
-    // cacheamos CSS de AOS que por alguna razón no lo pilla del CDN con estrategia CacheFirst
-    workbox.routing.registerRoute(
-        workbox.strategies.staleWhileRevalidate()
+        workbox.strategies.StaleWhileRevalidate()
     );
 }
